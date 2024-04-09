027115fd425771debba3ce2f66edb00fa60288b9
Alejandro Rosero Vicuña <alejandroroserov@gmail.com>
2024-04-09 11:43:39 +0200

    Add git config to avoid terminal lock when branches are listed with Less.

--- linux/setup_mac.sh
+++ linux/setup_mac.sh
@@ -22,3 +22,6 @@ sh install.sh
 
 # Install omz theme powerlevel 10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+git config --global --replace-all core.pager "less -F -X"
